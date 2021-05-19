d69f3195d5a52248f11372c11d73dce622448c5d
zy_nitro <zyascend@qq.com>
2021-05-19 21:06:10 +0800

    上传组件基础

---
+++ src/components/UpLoad.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='container'>
+  </div>
+</template>
+<script lang='ts'>
+import { defineComponent, toRefs, reactive } from 'vue'
+export default defineComponent({
+  name: 'Upload',
+  components: {},
+  props: {},
+  setup (props) {
+    const data = reactive({
+    })
+    const refData = toRefs(data)
+    return {
+      ...refData
+    }
+  }
+})
+</script>
+<style lang='scss' scoped>
+.container{
+}
+</style>
