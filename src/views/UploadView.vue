d3f18ff40da4de76e8e1fa2f16496ca2e8ae2a32
zy_nitro <zyascend@qq.com>
2021-05-29 13:48:57 +0800

    添加进度监听

--- src/views/UploadView.vue
+++ src/views/UploadView.vue
@@ -5,7 +5,7 @@
   </div>
 </template>
 <script lang='ts'>
-import { defineComponent, toRefs, reactive } from 'vue'
+import { defineComponent, toRefs, reactive, computed } from 'vue'
 import Upload from '@/components/UpLoad.vue'
 
 enum Status {
@@ -17,7 +17,15 @@ interface ReqAttr {
   url: string,
   method: string,
   data: FormData | string,
-  headers: Record<string, any>
+  headers: Record<string, any>,
+  onProgress: (this: XMLHttpRequest, ev: ProgressEvent) => any
+}
+
+interface ChunkData {
+  hash: string,
+  chunk: Blob,
+  progress: number,
+  size: number
 }
 const CHUNK_SIZE = 10 * 1024 * 1024 // 切片大小
 
@@ -31,27 +39,37 @@ export default defineComponent({
     const data = reactive({
       file: {} as FileList,
       status: Status.Waiting,
-      hashs: [''],
-      chunkList: [] as Array<Blob>
+      chunks: new Array<ChunkData>()
+    })
+    const uploadPercentage = computed(() => {
+      if (!data.file || !data.file.length || !data.chunks.length) return 0
+      const loaded = data.chunks
+        .map(item => item.size * item.progress)
+        .reduce((acc, cur) => acc + cur)
+      return parseInt((loaded / data.file[0].size).toFixed(2))
     })
     const refData = toRefs(data)
-    const createChunkList = (file: FileList, size: number): Array<Blob> => {
-      const chunkList = []
-      let curSize = 0
-      while (curSize < file[0].size) {
-        chunkList.push(file[0].slice(curSize, curSize + size))
-        curSize += size
-      }
-      return chunkList
-    }
 
-    const createHash = (chunkList: Array<Blob>) => {
-      const hashs = Array.from({ length: chunkList.length }, index => `${index}`)
-      return hashs
+    const sliceChunk = (files: FileList, size: number) => {
+      let curSize = 0
+      let index = 0
+      const filename = files[0].name
+      while (curSize < files[0].size) {
+        const f = files[0].slice(curSize, curSize + size)
+        data.chunks.push({
+          chunk: f,
+          hash: `${filename}-${index}`,
+          progress: 0,
+          size: f.size
+        })
+        curSize += size
+        index++
+      }
     }
     const request = (attrs: ReqAttr) => {
       return new Promise(resolve => {
         const xhr = new XMLHttpRequest()
+        xhr.upload.onprogress = attrs.onProgress
         xhr.open(attrs.method, attrs.url)
         Object.keys(attrs.headers).forEach(key => {
           xhr.setRequestHeader(key, attrs.headers[key])
@@ -73,21 +91,28 @@ export default defineComponent({
         method: 'post',
         headers: {
           'content-type': 'application/json'
-        }
+        },
+        onProgress: ev => ev
       })
     }
+    const getProgressHandlder = (index: number) => {
+      return (ev: ProgressEvent) => {
+        data.chunks[index].progress = parseInt(String((ev.loaded / ev.total) * 100))
+      }
+    }
     const uploadChunks = async () => {
-      const reqList = data.chunkList.map((chunk, index) => {
+      const reqList = data.chunks.map((item) => {
         const formData = new FormData()
-        formData.append('chunk', chunk)
-        formData.append('hash', data.hashs[index])
+        formData.append('chunk', item.chunk)
+        formData.append('hash', item.hash)
         formData.append('filename', data.file[0].name)
         return formData
-      }).map(formData => request({
+      }).map((formData, index) => request({
         url: 'http://localhost:3000',
         data: formData,
         method: 'post',
-        headers: {}
+        headers: {},
+        onProgress: getProgressHandlder(index)
       }))
       await Promise.all(reqList)
       await notifyMerge()
@@ -102,16 +127,15 @@ export default defineComponent({
       if (!data.file) return
       data.status = Status.Uploading
       // 生成文件切片
-      data.chunkList = createChunkList(data.file, CHUNK_SIZE)
-      // 生成每个切片的hash
-      data.hashs = createHash(data.chunkList)
+      sliceChunk(data.file, CHUNK_SIZE)
       // 上传切片
       await uploadChunks()
     }
     return {
       ...refData,
       handleFileChange,
-      handleUpload
+      handleUpload,
+      uploadPercentage
     }
   }
 })
